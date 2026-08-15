b12a3bd6180059ca5b3a6dcfc1917b36d3a36555
Arjun Aditya <arjunaditya92006@gmail.com>
2026-08-15 19:24:20 +0530

    fix err

--- workers/app.ts
+++ workers/app.ts
@@ -2,12 +2,19 @@ import { createRequestHandler } from "@react-router/cloudflare";
 // @ts-ignore
 import * as build from "../build/server/index.js";
 
-const requestHandler = createRequestHandler(build as any);
+const requestHandler = createRequestHandler({ build: build as any });
 
 export default {
-  async fetch(request: Request, env: any, ctx: any) {
-    return requestHandler(request, {
-      cloudflare: { env, ctx },
+  async fetch(request: Request, env: any, ctx: ExecutionContext) {
+    return requestHandler({
+      request,
+      env,
+      ctx,
+      waitUntil: ctx.waitUntil.bind(ctx),
+      passThroughOnException: ctx.passThroughOnException
+        ? ctx.passThroughOnException.bind(ctx)
+        : () => {},
     });
   },
 };
+
